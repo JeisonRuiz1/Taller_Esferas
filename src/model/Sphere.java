267133f78211893c9c43c9df16c22da416a148e0
JeisonRuiz1 <JeisonRuiz1910@gmail.com>
2021-03-04 13:40:26 -0500

    Clase sphere definida
    Se creo la clase Shpere en donde  el objeto contiene su correspondiente vector posicion, radio, diametro y volumen calculado.

--- src/model/Sphere.java
+++ src/model/Sphere.java
@@ -4,16 +4,57 @@ import java.io.BufferedReader;
 
 public class Sphere {
 
-    private double radio, area, volumen;
-    private double PI = 3.1416;
+    public static final double PI = Math.PI;
+    private double[] positions, speedct;
+    private double diameter, radio, volume;
 
     public Sphere(){
-        radio = (double)(Math.random());
-        System.out.println("El radio de la esfera es : " + radio);
-        area = PI * (radio*radio);
-        System.out.println("El area de la esfera es : " + area);
-        volumen = 4*PI*(radio*radio);
-        System.out.println("El volumen de la esfera es : " + volumen);
+        positions = new double[3];
+        assingDiameter();
+        calculateRadio();
+        calculateVolume();
+        assingPosition(positions);
+  /*      System.out.println("El diametro de su esfera es de: "+ diameter);
+        System.out.println("El radio de su esfera es de: "+ radio);
+        System.out.println("El volumen de su esfera es de: "+ volume);
+        System.out.println("La posiscion en X de su esfera es : " + positions[0]);
+        System.out.println("La posiscion en Y de su esfera es : " + positions[1]);
+        System.out.println("La posiscion en Z de su esfera es : "+ positions[2]);*/
+
+    }
+
+    /*
+    * @params
+    * asigna una posicion aleatoria para los ejes x,y,z en un area aleatoria de 500 unidades
+    * */
+    public void assingPosition(double[] positions){
+        positions[0] = Math.random()*500+1;
+        positions[1] = Math.random()*500+1;
+        positions[2] = Math.random()*500+1;
+    }
+
+    /*
+     * @params
+     * asigna un diametro aleatorio a la esfera entre 1 y 100 unidades.
+     * */
+    public void assingDiameter(){
+        diameter = Math.random()*100+1;
+    }
+
+    /*
+     * @params
+     * calcaula el radio de la esfera a partir de su diametro
+     * */
+    public void calculateRadio(){
+        radio = diameter/2;
+    }
+
+    /*
+     * @params
+     * calcula el volumen de la esfera a partir del radio con la formula basica.
+     * */
+    public void calculateVolume(){
+        volume = (4/3)*PI*(radio*radio*radio);
     }
 
     public static void main(String[] args) {
